0454112ddc24e3b598aea0f96054e403fa0f8c99
junn34 <lucasorjh3412@gmail.com>
2025-11-07 14:29:34 +0900

    dfad

--- src/main/java/dev/woori/wooriLearn/HelloController.java
+++ src/main/java/dev/woori/wooriLearn/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello World!!!!!!@@@ㄴd@ㅁ@@ㄴㅇㄹㄴㅁㅇㄹ!!@@@";
+        return "Hello World!!!!!!@@@ㄴd@ㅁㅇㄹㅇㄹ@@ㄴㅇㄹㄴㅁㅇㄹ!!@@@";
     }
 }
